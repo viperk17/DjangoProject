0bdc67dca77cb35ff849a945d1d894dd9122a679
Prashant Singh <pk.airsqn@gmail.com>
2020-07-18 13:33:08 +0530

    model registered

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. It's a kind of table in the database.
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    phone = models.CharField(max_length=12)
+    desc = models.TextField()
+    date = models.DateField()
